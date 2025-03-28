9df5bef1cf5e2570710a32089f76962121b89ff3
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-28 16:11:53 +0100

    Add data fetching and table display

---
+++ README.md
@@ -0,0 +1,2 @@
+Implementare la funzionalità di cancellazione:
+Aggiungi un pulsante nella tabella per ciascun post, al click del quale dovrai inviare una chiamata all'endpoint predisposto lato server per la rimozione di una risorsa.
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import {useState, useEffect} from 'react'
 
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
 
+const url = 'http://localhost:3000'
+
 function App() {
+
+  function  fetchData() {
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+      
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <>
