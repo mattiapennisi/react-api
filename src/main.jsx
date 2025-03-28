cc65b6e301d8f33539428c0f8403118e04a0e499
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-28 15:27:21 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/resets.css'
+import './css/utilities.css'
+import './css/media.css'
+import './index.css'
+import './App.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
