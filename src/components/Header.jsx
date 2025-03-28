c114fa4aa9397281fa6698857515112e0e4c4ca7
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-28 17:26:39 +0100

    Add delete functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header id="header">
-            <h1 className="h1 text-center mt-3 mb-5">Blog Posts</h1>
+            <h1 className="h1 text-center mt-3 mb-5">Blog Posts Dashboard</h1>
         </header>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,23 @@
 export default function Main({ posts, setPosts }) {
+    function handleDelete(slug) {
+        fetch(`http://localhost:3000/posts/${slug}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (res.ok) {
+                    setPosts(posts.filter(post => post.slug !== slug))
+                } else {
+                    throw new Error('Delete failed')
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error)
+            })
+    }
+
     return (
         <main id="main">
             <div className="table-responsive">
@@ -15,7 +34,7 @@ export default function Main({ posts, setPosts }) {
                             <tr key={post.slug}>
                                 <td scope="row">{post.title}</td>
                                 <td>{post.image}</td>
-                                <td><i className="fa-solid fa-trash"></i></td>
+                                <td><i className="fa-solid fa-trash" onClick={(e) => handleDelete(post.slug)}></i></td>
                             </tr>
                         ))}
                     </tbody>
